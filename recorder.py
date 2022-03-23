497ddb695c6b1e91383eda0c2ff40e5135535744
Daniel Schellenberg <daniel.schellenberg@piobyte.de>
2022-03-23 16:17:55 +0100

    first successful recording

--- recorder.py
+++ recorder.py
@@ -12,30 +12,30 @@ Usage:
 
 Options:
   -h --help             Show this screen.
-  --filename=<name>     Name of recording [default: myRadio].
+  --filename=<name>     Name of recording [default: record.mp3].
   --duration=<time>     Duration of recording in seconds [default: 30].
   --block_size=<size>    Block size for read/write in bytes [default:64].
   -l --list             List all recordings.
 """
-import requests
+
+import datetime as dt
+import urllib.request as rq
 
 from docopt import docopt
 
 
-def record(url='http://radios.rtbf.be/brf1-128.mp3', filename='record.mp3', blocksize=1024):
-    r = requests.get(url, stream=True)
+def record(url='http://radios.rtbf.be/musiq3-128.mp3', filename='record.mp3', blocksize=64, duration=30):
+    r = rq.urlopen(url)
+    start = dt.datetime.now()
 
     with open(filename, 'wb') as f:
-        try:
-            for block in r.iter_content(blocksize):
-                f.write(block)
-        except KeyboardInterrupt:
-            pass
+        while (dt.datetime.now() - start).seconds < duration:
+            f.write(r.read(blocksize))
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='cli_recorder 0.1')
+    args = docopt(__doc__, version='cli_recorder 0.1')
 
-    record()
+    record(url=args['<url>'], filename=args['--filename'], blocksize=args['--blocksize'])
 
-    print(arguments)
+    print(args)
