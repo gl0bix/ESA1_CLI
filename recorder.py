575df2ea938721b6e12ee41c6dbe678567ddb60a
Daniel Schellenberg <daniel.schellenberg@piobyte.de>
2022-03-23 17:20:44 +0100

    recording and list

--- recorder.py
+++ recorder.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3.9
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 __author__ = 'schellenberg'
 
 __doc__ = """\
 Usage:
-  recorder.py 
   recorder.py -l | --list
   recorder.py <url> [--filename=<name>] [--duration=<time>] [--blocksize=<size>]
   recorder.py -h | --help
@@ -14,13 +13,16 @@ Options:
   -h --help             Show this screen.
   --filename=<name>     Name of recording [default: record.mp3].
   --duration=<time>     Duration of recording in seconds [default: 30].
-  --block_size=<size>    Block size for read/write in bytes [default:64].
+  --blocksize=<size>   Block size for read/write in bytes [default: 300].
   -l --list             List all recordings.
 """
 
 import datetime as dt
+import os
 import urllib.request as rq
 
+from os import listdir
+from os.path import isfile, join
 from docopt import docopt
 
 
@@ -35,7 +37,17 @@ def record(url='http://radios.rtbf.be/musiq3-128.mp3', filename='record.mp3', bl
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='cli_recorder 0.1')
-
-    record(url=args['<url>'], filename=args['--filename'], blocksize=args['--blocksize'])
-
+    path = os.path.dirname(__file__)
     print(args)
+
+    if args['<url>'] is not None:
+        record(url=args['<url>'],
+               filename=args['--filename'],
+               blocksize=int(args['--blocksize']),
+               duration=int(args['--duration']))
+
+    if args.get('--list'):
+        files = [f for f in listdir(path) if isfile(join(path, f)) and f[-4:] == '.mp3']
+        print(*files)
+
+
